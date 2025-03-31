b7e2478c95368861868c7aaf103ced72c9416269
ima-69 <imansha.idr@gmail.com>
2025-03-31 14:55:30 +0530

    completed the adminProductSlice

--- frontend/src/redux/slices/adminProductSlice.js
+++ frontend/src/redux/slices/adminProductSlice.js
@@ -61,3 +61,51 @@ export const deleteProduct = createAsyncThunk(
         return id;
     }
 );
+
+const adminProductSlice = createSlice({
+    name: "adminProducts",
+    initialState: {
+        products: [],
+        loading: false,
+        error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchAdminProducts.pending, (state) => {
+            state.loading = true;
+        })
+        .addCase(fetchAdminProducts.fulfilled, (state, action) => {
+            state.loading = false;
+            state.products = action.payload;
+        })
+        .addCase(fetchAdminProducts.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+        })
+
+        // Create Product
+        .addCase(createProduct.fulfilled, (state, action) => {
+            state.products.push(action.payload);
+        })
+
+        // Update Product
+        .addCase(updateProduct.fulfilled, (state, action) => {
+            const index = state.products.findIndex(
+                (product) => product._id === action.payload._id
+            );
+            if (index !== -1) {
+                state.products[index] = action.payload;
+            }
+        })
+
+        // Delete Products
+        .addCase(deleteProduct.fulfilled, (state, action) => {
+            state.products = state.products.filter(
+                (product) => product._id !== action.payload
+            );
+        });
+    },
+});
+
+export default adminProductSlice.reducer;
