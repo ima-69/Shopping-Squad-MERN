03556c15bd55360445260ebfdf64cbf84ed97fa8
ima-69 <imansha.idr@gmail.com>
2025-03-31 15:15:30 +0530

    created functions for fetch all orders, update and delete order in admin orderSlice

--- frontend/src/redux/slices/adminOrderSlice.js
+++ frontend/src/redux/slices/adminOrderSlice.js
@@ -20,3 +20,44 @@
         }
     }
  );
+
+// update order delivery status
+export const updateOrderStatus = createAsyncThunk(
+    "adminOrders/updateOrderStatus",
+    async ({ id, state }, {rejectWithValue}) => {
+        try {
+            const response = await axios.put(
+                `${import.meta.env.VITE_BACKEND_URL}/api/admin/orders/${id}`, 
+                {status},
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+                    },
+                }
+            );
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+);
+
+// Delete an order
+export const deleteOrder = createAsyncThunk(
+    "adminOrders/deleteOrder",
+    async ( id, {rejectWithValue}) => {
+        try {
+            await axios.delete(
+                `${import.meta.env.VITE_BACKEND_URL}/api/admin/orders/${id}`, 
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+                    },
+                }
+            );
+            return id;
+        } catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+);
