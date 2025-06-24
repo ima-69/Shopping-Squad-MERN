074dc9cf0d0b13a67b078663b950f53dd3ebff50
ima-69 <imansha.idr@gmail.com>
2025-06-25 01:39:20 +0530

    Create adminProductController.js

---
+++ backend/controllers/adminProductController.js
@@ -0,0 +1,12 @@
+const Product = require("../models/Product");
+
+// @desc    Get all products (Admin only)
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
