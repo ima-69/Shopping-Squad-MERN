1f53b815f19d64cff70d465e9b78b5ca20709995
ima-69 <imansha.idr@gmail.com>
2025-06-25 00:58:35 +0530

    restructured checkout  routes with controller

---
+++ backend/controllers/checkoutController.js
@@ -0,0 +1,101 @@
+// controllers/checkoutController.js
+
+const Checkout = require("../models/Checkout");
+const Cart = require("../models/Cart");
+const Order = require("../models/Order");
+
+// @desc Create a new checkout session
+exports.createCheckoutSession = async (req, res) => {
+  const { checkoutItems, shippingAddress, paymentMethod, totalPrice } = req.body;
+
+  if (!checkoutItems || checkoutItems.length === 0) {
+    return res.status(400).json({ message: "no items in checkout" });
+  }
+
+  try {
+    const newCheckout = await Checkout.create({
+      user: req.user._id,
+      checkoutItems : checkoutItems,
+      shippingAddress,
+      paymentMethod,
+      totalPrice,
+      paymentStatus: "Pending",
+      isPaid: false,
+    });
+    console.log(`Checkout created for user: ${req.user._id}`);
+    res.status(201).json(newCheckout);
+  } catch (error) {
+    console.error("Error Creating checkout session:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// @desc Update checkout to mark as paid after successful payment
+exports.updateCheckoutPaid = async (req, res) => {
+  const { paymentStatus, paymentDetails } = req.body;
+
+  try {
+    const checkout = await Checkout.findById(req.params.id);
+
+    if (!checkout) {
+      return res.status(404).json({ message: "Checkout not found" });
+    }
+
+    if (paymentStatus === "paid") {
+      checkout.isPaid = true;
+      checkout.paymentStatus = paymentStatus;
+      checkout.paymentDetails = paymentDetails;
+      checkout.paidAt = Date.now();
+      await checkout.save();
+
+      res.status(200).json(checkout);
+    } else {
+      res.status(400).json({ message: "Invalid Payment Status" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// @desc Finalize checkout and convert to an order after payment confirmation
+exports.finalizeCheckout = async (req, res) => {
+  try {
+    const checkout = await Checkout.findById(req.params.id);
+
+    if (!checkout) {
+      return res.status(404).json({ message: "Checkout not found" });
+    }
+
+    if (checkout.isPaid && !checkout.isFinalized) {
+      const finalOrder = await Order.create({
+        user: checkout.user,
+        orderItems: checkout.checkoutItems,
+        shippingAddress: checkout.shippingAddress,
+        paymentMethod: checkout.paymentMethod,
+        totalPrice: checkout.totalPrice,
+        isPaid: true,
+        paidAt: checkout.paidAt,
+        isDelivered: false,
+        paymentStatus: "paid",
+        paymentDetails: checkout.paymentDetails,
+      });
+
+      checkout.isFinalized = true;
+      checkout.finalizedAt = Date.now();
+      await checkout.save();
+
+      // Delete the cart associated with the user
+      await Cart.findOneAndDelete({ user: checkout.user });
+
+      res.status(201).json(finalOrder);
+    } else if (checkout.isFinalized) {
+      res.status(400).json({ message: "Checkout already finalized" });
+    } else {
+      res.status(400).json({ message: "Checkout is not paid" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
