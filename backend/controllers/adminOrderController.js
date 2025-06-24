8108ee675861f3039aa7bcb10b9389dfe170e85b
ima-69 <imansha.idr@gmail.com>
2025-06-25 00:22:08 +0530

    restructured adminOrders routes with controller

---
+++ backend/controllers/adminOrderController.js
@@ -0,0 +1,52 @@
+// controllers/adminOrderController.js
+
+const Order = require("../models/Order");
+
+// @desc Get all orders (Admin only)
+exports.getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({}).populate("user", "name email");
+    res.json(orders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// @desc Update order status
+exports.updateOrderStatus = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id).populate("user", "name");
+    if (order) {
+      order.status = req.body.status || order.status;
+      order.isDelivered =
+        req.body.status === "Delivered" ? true : order.isDelivered;
+      order.deliveredAt =
+        req.body.status === "Delivered" ? Date.now() : order.deliveredAt;
+
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404).json({ message: "Order not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+// @desc Delete an order
+exports.deleteOrder = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      await order.deleteOne();
+      res.json({ message: "Order removed" });
+    } else {
+      res.status(404).json({ message: "Order not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
